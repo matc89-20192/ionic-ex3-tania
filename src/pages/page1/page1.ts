51777568b97db92d806e021f120bc96ed7f805b8
taniacristina <roc.ta.cris@gmail.com>
2019-11-27 10:35:22 -0300

    Adicionando itens a lista

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,8 +8,38 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController) {
+  descricao: string = "";
+  prioridade: number;
+  lista: Array<{desc: string, priori: number}> = [];
 
+  constructor(public navCtrl: NavController, public toast: ToastController) {
+    this.lista = [];
+  }
+
+  adicionar() {
+      console.log("cheguei auqi");
+      this.lista.push({
+        desc: this.descricao,
+        priori: this.prioridade,
+      });
+      var btnRemove = <HTMLButtonElement>document.getElementById('buttonRemover');
+      btnRemove.disabled = false;
+
+      this.lista.sort((taskA, taskB) => {
+        if (taskA.priori < +taskB.priori || taskA.priori == +taskB.priori) return -1;
+        return 1;
+      });
+
+      this.descricao = "";
+      this.prioridade = null;
+  }
+
+  remover() {
+
+  }
+
+  tarefa(i) {
+    
   }
 
 }
