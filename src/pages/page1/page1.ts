1f436002b71f26acfc36a5cbc6636b57795faa44
taniacristina <roc.ta.cris@gmail.com>
2019-11-27 11:03:17 -0300

    Adicionando validação de tarefa e ordenação por tempo de inserção

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -10,27 +10,32 @@ export class Page1 {
 
   descricao: string = "";
   prioridade: number;
-  lista: Array<{desc: string, priori: number}> = [];
+  tempo: number = 0;
+  lista: Array<{desc: string, priori: number, tempo: number}> = [];
 
   constructor(public navCtrl: NavController, public toast: ToastController) {
     this.lista = [];
   }
 
   adicionar() {
+    if(this.validar()){
+      this.tempo += 1;
       this.lista.push({
         desc: this.descricao,
         priori: this.prioridade,
+        tempo: this.tempo
       });
       var btnRemove = <HTMLButtonElement>document.getElementById('buttonRemover');
       btnRemove.disabled = false;
 
-      this.lista.sort((taskA, taskB) => {
-        if (taskA.priori < taskB.priori || taskA.priori == taskB.priori) return -1;
+      this.lista.sort((tarefa1, tarefa2) => {
+        if (tarefa1.priori < tarefa2.priori || tarefa1.priori == tarefa2.priori && tarefa1.tempo < tarefa2.tempo) return -1;
         return 1;
       });
 
       this.descricao = "";
       this.prioridade = null;
+    }
   }
 
   remover() {
@@ -41,6 +46,28 @@ export class Page1 {
     }
   }
 
+  validar(){
+    if(this.prioridade < 0 || this.prioridade > 10) {
+      var toast = this.toast.create({
+        message: "A prioridade deve estar entre 1 e 10.",
+        duration: 3000
+      });
+      toast.present();
+      return false;
+    } else {
+      for(var l of this.lista) {
+        if (this.descricao == l.desc) {
+          var toast = this.toast.create({
+            message: "Tarefa já cadastrada.",
+            duration: 3000
+          });
+          toast.present();
+          return false;
+        }
+      }
+    }
+    return true;
+  }
   tarefa(i) {
     
   }
